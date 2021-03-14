958106e76be988f84098eab27b26359e7c9f3c34
Jose Miguel Armijo <jose.miguel.armijo@sgdigital.com>
2021-03-14 21:08:11 +1100

    Show list of references to sources in person html Show notes in person html

--- src/web/builder.rb
+++ src/web/builder.rb
@@ -1,5 +1,4 @@
 require_relative 'file_manager'
-require_relative 'section'
 
 class Builder
     def initialize
@@ -21,5 +20,19 @@ class Builder
         directory = 'ind'
         file_name = "#{person.id.tr('@', '')}.html"
         @file_manager.save_page(page, directory, file_name)
+    end
+
+    def build_list(section, entities)
+        return '' if entities.length.zero?
+
+        template = @file_manager.open_template("#{section}.html")
+        return template.result(binding)
+    end
+
+    def build_section(section, entity)
+        return '' if entity.nil?
+
+        template = @file_manager.open_template("#{section}.html")
+        return template.result(binding)
     end
 end
