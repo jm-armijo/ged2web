441db955c5644fab3515a89d695da4f519889ad6
Jose Miguel Armijo <jose.miguel.armijo@sgdigital.com>
2021-03-28 01:34:55 +1100

    Recursively look for nested records

--- src/ged/record.rb
+++ src/ged/record.rb
@@ -1,5 +1,5 @@
 class Record
-    attr_writer :reference
+    attr_accessor :reference
 
     def initialize(line)
         @line = line
@@ -33,13 +33,30 @@ class Record
         return @records.find_all { |record| record.tag == tag }
     end
 
-    def deep_find_all(tag)
-        records_found = find_all(tag)
-
-        @records.each do |record|
-            records_found.concat(record.find_all(tag))
+    def deep_find_all(tag, depth = 0)
+        # Jump into one reference only to avoid circular dependency
+        if depth == 2
+            records_found = []
+        else
+            records_found = do_deep_find(tag, depth)
         end
 
-        return records_found.uniq.sort { |a, b| a.short_id <=> b.short_id }
+        return records_found.uniq(&:short_id).sort { |a, b| a.short_id <=> b.short_id }
+    end
+
+private
+
+    def do_deep_find(tag, depth)
+        records_found = find_all(tag)
+
+        # Recursivelly check next reference: increase depth
+        records_found.concat(reference.deep_find_all(tag, depth + 1)) if !reference.nil?
+
+        @records.each do |record|
+            # Check nested record: pass current depth
+            records_found.concat(record.deep_find_all(tag, depth))
+        end
+
+        return records_found
     end
 end
