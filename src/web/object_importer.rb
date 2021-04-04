88269b928e08357b83584861980c8ed7ab189b3a
Jose Miguel Armijo <jose.miguel.armijo@sgdigital.com>
2021-04-04 18:52:32 +1000

    Add styles for person page

--- src/web/object_importer.rb
+++ src/web/object_importer.rb
@@ -6,6 +6,13 @@ class ObjectImporter
     end
 
     def import
+        import_objects
+        import_dependencies
+    end
+
+private
+
+    def import_objects
         directory = './out/obj/'
         FileUtils.mkdir_p(directory)
 
@@ -15,5 +22,12 @@ class ObjectImporter
             new_path = "#{directory}/#{instance.short_id}.#{instance.format}"
             FileUtils.cp(instance.file, new_path)
         end
+    end
+
+    def import_dependencies
+        directory = './out/css/'
+        FileUtils.mkdir_p(directory)
+
+        FileUtils.cp('css/ind.css', './out/css/')
     end
 end
