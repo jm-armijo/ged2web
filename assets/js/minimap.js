a8b8bc56d980e82ff02e27dbc10ae12a5c448352
Jose Miguel Armijo <jose.miguel.armijo@sgdigital.com>
2021-05-21 22:16:02 +1000

    Add common styles Add styles for tree

--- assets/js/minimap.js
+++ assets/js/minimap.js
@@ -5,8 +5,8 @@ pagemap(document.querySelector('#map'), {
 			'h1,a': 'rgba(0,0,0,1)',
 			'h2,h3,h4': 'rgba(0,0,0,1)'
 		    },
-	back: 'rgba(205,205,205,1)',
-	view: 'rgba(255,255,255,0.50)',
-	drag: 'rgba(255,255,255,0.50)',
+	back: 'rgba(222,200,144,1.00)',
+	view: 'rgba(203,167,77,0.50)',
+	drag: 'rgba(203,167,77,0.50)',
 	interval: null
 });
