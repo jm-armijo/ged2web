a9c383c297a6a24b7e12bbc6ede32fb7eaa1c45b
Jose Miguel Armijo <jose.miguel.armijo@sgdigital.com>
2021-06-12 01:27:45 +1000

    Change format of banner Change colors and location of minimap Change styles of links to sections (tree, list) Add color to lines connecting people

--- assets/js/minimap.js
+++ assets/js/minimap.js
@@ -1,12 +1,10 @@
 pagemap(document.querySelector('#map'), {
 	viewport: null,
 	styles: {
-			'header,footer,section,article': 'rgba(0,0,0,0.20)',
-			'h1,a': 'rgba(0,0,0,1)',
-			'h2,h3,h4': 'rgba(0,0,0,1)'
+			'a': 'rgba(255,255,255,1)',
 		    },
-	back: 'rgba(63,65,93,1.00)',
-	view: 'rgba(28,34,64,0.50)',
-	drag: 'rgba(28,34,64,0.50)',
+	back: 'rgba(116,0,128,1.00)',
+	view: 'rgba(19,19,70,0.50)',
+	drag: 'rgba(19,19,70,0.50)',
 	interval: null
 });
